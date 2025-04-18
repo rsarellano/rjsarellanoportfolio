c64c26a1b5953be17e7a4e91206aa0b9b6336586
rsarellano <rjsarellano@gmail.com>
2025-04-19 05:25:22 +0900

    added card button for projects

--- rjsarellanoportfolio/src/components/projects/corporate/corporateProjects.tsx
+++ rjsarellanoportfolio/src/components/projects/corporate/corporateProjects.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const CorporateProjects = () => {
   return (
-    <div>Corporate Projects</div>
+    <div>Corporate Projects
+
+      <div>
+      <div className="card">
+    <div className="icon">
+    </div>
+
+    <div className="content">
+        <h3>Title</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur </p>
+    </div>
+</div>
+      </div>
+    </div>
   )
 }
 
