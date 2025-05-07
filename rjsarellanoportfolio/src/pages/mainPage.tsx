6c914c846ea1113fce263b6363c5f3e8e6bb4694
rsarellano <rjsarellano@gmail.com>
2025-05-07 08:54:36 +0800

    added navs

--- rjsarellanoportfolio/src/pages/mainPage.tsx
+++ rjsarellanoportfolio/src/pages/mainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import CorporateProjects from "../components/projects/corporate/corporateProjects";
 import PersonalProjects from "../components/projects/personal/personalProjects";
 import Header from "../components/header/header";
@@ -19,38 +19,70 @@ const MainPage = () => {
     setShowCorporateProjects(false);
   };
 
+  const aboutMe = useRef(null);
+  const services = useRef(null);
+  const projects = useRef(null);
+
+  const scrollToSection = (ref: any) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div>
+    <div className="relative w-[1000px]">
+      <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-[1000px] bg-gray-800 my-2 text-black py-4 z-50 flex justify-center space-x-6">
+        <button
+          onClick={() => scrollToSection(aboutMe)}
+          className="hover:underline"
+        >
+          About Me
+        </button>
+
+        <button
+          onClick={() => scrollToSection(services)}
+          className="hover:underlined"
+        >
+          Services
+        </button>
+
+        <button
+          onClick={() => scrollToSection(projects)}
+          className="hover:underlined"
+        >
+          Projects
+        </button>
+      </nav>
       <div>
         <Header />
       </div>
-      <div>
+      <section ref={aboutMe}>
         <AboutMe />
-      </div>
+      </section>
+      <section ref={services}>
+        <Services />
+      </section>
 
-      <Services />
+      <section ref={projects}>
+        <div className="container bg-sky-600 mb-2 position: relative">
+          <div className="h-[900px] ">
+            <button
+              className="m-5 w-[120px] h-[80px]"
+              onClick={toggleCorporateProjects}
+            >
+              {showCorporateProjects}
+              Corporate Projects
+            </button>
+            <button
+              className="m-5 w-[120px] h-[80px]"
+              onClick={togglePersonalProjects}
+            >
+              Personal Projects
+            </button>
 
-      <div className="container bg-sky-600 mb-2 position: relative">
-        <div className="h-[800px] w-[900px]">
-          <button
-            className="m-5 w-[120px] h-[80px]"
-            onClick={toggleCorporateProjects}
-          >
-            {showCorporateProjects}
-            Corporate Projects
-          </button>
-          <button
-            className="m-5 w-[120px] h-[80px]"
-            onClick={togglePersonalProjects}
-          >
-            {" "}
-            Personal Projects{" "}
-          </button>
-
-          {showPersonalProjects && <PersonalProjects />}
-          {showCorporateProjects && <CorporateProjects />}
+            {showPersonalProjects && <PersonalProjects />}
+            {showCorporateProjects && <CorporateProjects />}
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
