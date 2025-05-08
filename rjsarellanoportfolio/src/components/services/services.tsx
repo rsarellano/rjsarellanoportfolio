cc1c8c45dc6bfa105bc0e970b24db9437e9e25f8
rsarellano <rjsarellano@gmail.com>
2025-05-08 10:52:20 +0800

    added web Development service

--- rjsarellanoportfolio/src/components/services/services.tsx
+++ rjsarellanoportfolio/src/components/services/services.tsx
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
+import WebDevelopment from "./webDevelopment";
 
 const Services = () => {
   const [moveLeft, setMoveLeft] = useState(false);
 
   return (
-    <div className="flex justify-center h-[900px] bg-sky-200">
-      <div className="relative w-full h-auto bg-gray-100">
+    <div className="flex justify-center h-[1400px] bg-sky-200">
+      <div className="relative w-full h-[1400px] bg-gray-100">
         <div
           className={`
-            absolute top-[50px] bottom-[50px] left-2/6 w-px bg-black
+            absolute top-[100px] bottom-[50px] left-2/6 w-px bg-black
             transform transition-transform duration-700
             ${moveLeft ? "-translate-x-[300px]" : "-translate-x-1/2"}
           `}
         >
           <div className="absolute top-0 left-4 flex flex-col space-y-[150px]">
-            <button
-              onClick={() => setMoveLeft(true)}
-              className="px-4 py-2 bg-blue-600 text-black rounded shadow"
-            >
-              Web Development
-            </button>
-            <button className="px-4 py-2 bg-blue-600 text-black rounded shadow">
-              QA Automation
-            </button>
-            <button className="px-4 py-2 bg-blue-600 text-black rounded shadow">
-              Support
-            </button>
+            <div className="h-[400px]">
+              <button
+                onClick={() => setMoveLeft(true)}
+                className="px-4 py-2 bg-blue-600 text-black rounded shadow"
+              >
+                Web Development
+              </button>
+              <div>
+                <WebDevelopment />
+              </div>
+            </div>
+            <div>
+              <button className="px-4 py-2 bg-blue-600 text-black rounded shadow">
+                QA Automation
+              </button>
+            </div>
+            <div>
+              <button className="px-4 py-2 bg-blue-600 text-black rounded shadow">
+                Support
+              </button>
+            </div>
           </div>
         </div>
       </div>
---
+++ rjsarellanoportfolio/src/components/services/webDevelopment.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const webDevelopment = () => {
+  return (
+    <div>
+      webDevelopment
+      <div className="webDevelopmentCards  bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
+        <h3>Information Gathering</h3>
+      </div>
+    </div>
+  );
+};
+
+export default webDevelopment;
