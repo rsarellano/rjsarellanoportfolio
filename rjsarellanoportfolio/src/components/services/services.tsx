da2a6b644f4926f1ab0845e887b1fd87df985c50
rsarellano <rjsarellano@gmail.com>
2025-05-11 08:20:52 +0800

    added qa content function slide left upon click

--- rjsarellanoportfolio/src/components/services/services.tsx
+++ rjsarellanoportfolio/src/components/services/services.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import WebDevelopment from "./webDevelopment";
-import Qa from "./qa";
+import WebDevelopmentContent from "./webDevelopment";
+import QaContent from "./qa";
 
 const Services = () => {
   const [moveLeft, setMoveLeft] = useState(false);
@@ -8,33 +8,34 @@ const Services = () => {
   return (
     <div className="flex justify-center h-[1600px] bg-sky-200">
       <div className="relative w-full h-[1600px] bg-gray-100">
-        <div
-          className={`
+        <div className="absolute top-0 left-4 flex flex-col space-y-[150px]">
+          <div
+            className={` h-[400px]
             absolute top-[100px] bottom-[50px] left-2/6 w-px bg-black
             transform transition-transform duration-700
             ${moveLeft ? "-translate-x-[300px]" : "-translate-x-1/2"}
           `}
-        >
-          <div className="absolute top-0 left-4 flex flex-col space-y-[150px]">
-            <div className="h-[400px]">
-              <button
-                onClick={() => setMoveLeft(true)}
-                className="px-4 py-2 bg-blue-600 text-black rounded shadow"
-              >
-                Web Development
-              </button>
-              <div className="absolute mt-[180px]">
-                <WebDevelopment />
-              </div>
+          >
+            <button
+              onClick={() => setMoveLeft(true)}
+              className="px-4 py-2 bg-blue-600 text-black rounded shadow"
+            >
+              Web Development
+            </button>
+            <div className="absolute mt-[180px]">
+              <WebDevelopmentContent />
             </div>
-            <div>
-              <Qa />
-            </div>
-            <div>
-              <button className="px-4 py-2 bg-blue-600 text-black rounded shadow">
-                Support
-              </button>
-            </div>
+          </div>
+          <div
+            className={`h-[400px] absolute top-[100px] bottom-[50px] left-2/6 w-px bg-black transform transition-transform duration-700
+            ${moveLeft ? "-translate-x-[300px]" : "-translate-x-1/2"}`}
+          >
+            <QaContent />
+          </div>
+          <div>
+            <button className="px-4 py-2 bg-blue-600 text-black rounded shadow">
+              Support
+            </button>
           </div>
         </div>
       </div>
