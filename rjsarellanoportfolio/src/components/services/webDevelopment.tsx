ba0692780168326524cf8538e4ab23833e30348f
rsarellano <rjsarellano@gmail.com>
2025-05-26 21:50:26 +0800

    building timer for web development cards

--- rjsarellanoportfolio/src/components/services/webDevelopment.tsx
+++ rjsarellanoportfolio/src/components/services/webDevelopment.tsx
@@ -64,24 +64,26 @@ const webDevelopment = ({
 
   return (
     <div className="flex justify-center gap-8 overflow-hidden">
-      <div className=" webDevMainCard bg-red-500 h-[250px]  p-4 w-[200px] rounded">
+      <div className=" webDevMainCard bg-red-500 h-[260px]  p-4 w-[200px] rounded">
         <div className="h-[24px]">{isVisible && <h3>Planning Card</h3>}</div>
         {isVisibleDiagonalLine && (
           <div className="diagonalLine w-px h-12 bg-black absolute top-20 left-24 transform rotate-[220deg] origin-top z-0"></div>
         )}
 
         <div className="webDevelopmentCards h-[65px] w-[150px]  m-2 mt-[63px]  my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
+          
           {isVissibleMainCard && <h3>Planning </h3>}
         </div>
       </div>
 
       <div className="webDevMainCard bg-red-500 p-4 w-[200px] rounded">
         <div className="webDevelopmentCards h-[65px] w-[150px]  m-2 mt-[85px]  my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
+       
           <div>{isVissibleMainCard && <h3>Information Gathering</h3>}</div>
         </div>
 
         <div>
-          <h3>Information Gathering Card</h3>
+          {isVisible && <h3>Information Gathering Card</h3>}
         </div>
       </div>
 
