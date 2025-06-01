36d820680fb5308a2e4f698a35dabef1eb7f5c20
rsarellano <rjsarellano@gmail.com>
2025-06-01 23:19:22 +0800

    fixed QA alignment

--- rjsarellanoportfolio/src/components/services/qa.tsx
+++ rjsarellanoportfolio/src/components/services/qa.tsx
@@ -26,17 +26,17 @@ const Qa = ({triggerVisibleCardsQa,triggerVisibleSubCardsQa,triggerVisibleDiagon
   
   return (
     <div className="flex justify-center gap-1 overflow-hidden ml-[20px]">
-      <div className="webDevMainCard  bg-gray-500 w-[180px] rounded ">
+      <div className="webDevMainCard  bg-gray-500 w-[180px] h-[220px] rounded ">
         <div>Design Card</div>
         <div className="webDevelopmentCards h-[49px] w-[150px] m-2 mt-[63px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
-          <div>{visibleCardCount >= 1 && <h3>Manual QA</h3>}</div>
+          <div className=" h-[24px]">{visibleCardCount >= 1 && <h3>Manual QA</h3>}</div>
         </div>
       </div>
 
 
       <div className="webDevMainCard  bg-gray-500 w-[180px] rounded ">
        
-        <div className="webDevelopmentCards h-[49px] w-[150px] m-2 mt-[63px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
+        <div className="webDevelopmentCards h-[49px] w-[150px] m-2 mt-[87px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
           <div>{ visibleCardCount >= 2 && <h3>QA Automation</h3> } </div>
           
         </div>
