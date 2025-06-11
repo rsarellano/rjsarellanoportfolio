f22fe3f6fdebb48e9ee7d34566fc7e20751a3598
rsarellano <rjsarellano@gmail.com>
2025-06-11 08:51:29 +0800

    fixed QA card transition

--- rjsarellanoportfolio/src/components/services/qa.tsx
+++ rjsarellanoportfolio/src/components/services/qa.tsx
@@ -19,8 +19,8 @@ const Qa = ({triggerVisibleCardsQa,triggerVisibleSubCardsQa,triggerVisibleDiagon
   
   useEffect(() => {
   if(triggerVisibleCardsQa) {
-    setTimeout(() => setIsVisibleCardCount((prev) => Math.max(prev,1)), 550 )
-    setTimeout(() => setIsVisibleCardCount((prev) => Math.max(prev,2)), 590 )
+    setTimeout(() => setIsVisibleCardCount((prev) => Math.max(prev,1)), 520 )
+    setTimeout(() => setIsVisibleCardCount((prev) => Math.max(prev,2)), 620 )
   }
   },[triggerVisibleCardsQa]
   
@@ -39,21 +39,23 @@ const Qa = ({triggerVisibleCardsQa,triggerVisibleSubCardsQa,triggerVisibleDiagon
   return (
     <div className="flex justify-center gap-1 overflow-hidden ml-[20px]">
       <div className="QA Cards  bg-gray-500 w-[180px] h-[220px] rounded ">
-        <div><h3>Manual Testing sub Card</h3></div>
-        <div className="qaMainCards h-[49px] w-[150px] m-2 mt-[63px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
-          <div className=" h-[24px]">{visibleCardCount >= 1 && <h3>Manual QA</h3>}</div>
-        </div>
+       <div className="h-[24px]"> {visibleSubCardCount >= 1 && <h3>Manual Testing sub Card</h3> }</div> 
+        {visibleCardCount >= 1 &&  <div className="qaMainCards h-[49px] w-[150px] m-2 mt-[63px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
+        Manual Testing <div className=" h-[34px]"><h3>Manual QA</h3></div>
+        </div>}
       </div>
 
 
       <div className="QA Cards  bg-gray-500 w-[180px] rounded ">
        
-        <div className=" h-[49px] w-[150px] m-2 mt-[87px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
-        { visibleCardCount >= 2 &&  <div><h3>QA Automation</h3></div> } 
+      { visibleCardCount >= 2 &&  <div className=" h-[49px] w-[150px] m-2 mt-[87px] my-5 bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg overflow-hidden">
+       <div><h3>QA Automation</h3></div> 
           
-        </div>
-        <div>QA Automation sub Card</div>
+        </div>} 
+        {visibleSubCardCount >= 2 && <div><h3>QA Automation subcard</h3></div>}
       </div>
+
+      
     </div>
   );
 };
